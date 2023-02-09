18ee50f2cd4833bce5c763d42beab1bd52141b61
Davor <hyperv@ucione.local>
2023-02-09 21:13:35 +0100

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { Component } from "react";
 
-const App = () => {
-  return (
-    <div></div>
-  );
+class App extends Component {
+    state = {
+        users: [
+            {
+                ime: "Branko",
+                prezime: "Branković",
+                dob: 32,
+            },
+            {
+                ime: "Janko",
+                prezime: "Janković",
+                dob: 42,
+            },
+            {
+                ime: "Stanko",
+                prezime: "Stanković",
+                dob: 52,
+            },
+        ],
+    };
+
+    return() {
+        return <div></div>;
+    }
 }
-
 export default App;
